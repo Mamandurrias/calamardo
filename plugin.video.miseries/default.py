c537b540bb081860549c4651486016eb5c38746d
Mamandurrias <tu@email.com>
2026-03-23 17:15:46 +0000

    v1.0.5 - Fix iconos menú principal y categorías

--- plugin.video.miseries/default.py
+++ plugin.video.miseries/default.py
@@ -19,12 +19,15 @@ def menu_principal():
     db = get_series()
     tiene_colecciones = any(data.get('coleccion') for data in db.values())
 
-    item = xbmcgui.ListItem("🎭 Géneros")
+    # Opción Géneros con icono
+    item = xbmcgui.ListItem("Géneros")
+    item.setArt({'icon': 'DefaultGenre.png', 'thumb': 'DefaultGenre.png'})
     item.setInfo('video', {'title': 'Géneros', 'mediatype': 'tvshow'})
     xbmcplugin.addDirectoryItem(HANDLE, f"{sys.argv[0]}?action=generos", item, True)
 
     if tiene_colecciones:
-        item = xbmcgui.ListItem("📦 Colecciones")
+        item = xbmcgui.ListItem("Colecciones")
+        item.setArt({'icon': 'DefaultAddons.png', 'thumb': 'DefaultAddons.png'})
         item.setInfo('video', {'title': 'Colecciones', 'mediatype': 'tvshow'})
         xbmcplugin.addDirectoryItem(HANDLE, f"{sys.argv[0]}?action=colecciones", item, True)
 
@@ -42,6 +45,7 @@ def listar_generos():
         generos[genero].append(nombre)
     for genero in sorted(generos.keys()):
         item = xbmcgui.ListItem(genero)
+        item.setArt({'icon': 'DefaultGenre.png', 'thumb': 'DefaultGenre.png'})
         item.setInfo('video', {'title': genero, 'mediatype': 'tvshow'})
         url = f"{sys.argv[0]}?action=series_por_genero&genero={urllib.parse.quote(genero)}"
         xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
@@ -59,6 +63,7 @@ def listar_colecciones():
             colecciones[col].append(nombre)
     for col in sorted(colecciones.keys()):
         item = xbmcgui.ListItem(col)
+        item.setArt({'icon': 'DefaultAddons.png', 'thumb': 'DefaultAddons.png'})
         item.setInfo('video', {'title': col, 'mediatype': 'tvshow'})
         url = f"{sys.argv[0]}?action=series_por_coleccion&coleccion={urllib.parse.quote(col)}"
         xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
@@ -108,73 +113,63 @@ def listar_series_por_coleccion(coleccion):
 
 def listar_temporadas(serie):
     db = get_series()
-    data = db[serie]
-    temporadas = sorted(set(ep['temporada'] for ep in data['episodios']))
-    poster = data.get('poster', '')
-    for t in temporadas:
-        nombre = f"Temporada {int(t)}"
-        art = {'poster': poster, 'thumb': poster, 'icon': poster}
-        item = xbmcgui.ListItem(nombre)
-        item.setArt(art)
-        item.setInfo('video', {'title': nombre, 'mediatype': 'season', 'season': int(t)})
-        url = f"{sys.argv[0]}?action=episodios&serie={urllib.parse.quote(serie)}&temporada={t}"
+    data = db.get(serie, {})
+    temps = sorted(data.get('temporadas', []), key=lambda x: int(x))
+    for temp in temps:
+        item = xbmcgui.ListItem(f"Temporada {temp}")
+        item.setInfo('video', {'title': f"Temporada {temp}", 'mediatype': 'season'})
+        url = f"{sys.argv[0]}?action=episodios&serie={urllib.parse.quote(serie)}&temp={temp}"
         xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
     xbmcplugin.setContent(HANDLE, 'seasons')
     xbmcplugin.endOfDirectory(HANDLE)
 
-def listar_episodios(serie, temporada):
+def listar_episodios(serie, temp):
     db = get_series()
-    data = db[serie]
-    episodios = sorted(
-        [ep for ep in data['episodios'] if ep['temporada'] == temporada],
-        key=lambda x: x['episodio']
-    )
-    import base64
+    data = db.get(serie, {})
+    episodios = data.get('episodios', {}).get(temp, [])
     for ep in episodios:
-        titulo = ep['titulo']
-        poster = ep.get('poster', data.get('poster', ''))
-        url = ep['url']
-        if '.m3u8' in url:
-            stream_url = url
-        else:
-            encoded = base64.b64encode(url.encode()).decode()
-            stream_url = f"{BASE_URL}/play/{encoded}"
-        art = {'poster': poster, 'thumb': poster, 'icon': poster}
-        item = xbmcgui.ListItem(titulo)
-        item.setArt(art)
-        item.setInfo('video', {
-            'title': titulo,
-            'mediatype': 'episode',
-            'season': int(temporada),
-            'episode': int(ep['episodio']),
-            'plot': ep.get('sinopsis', '')
-        })
-        item.setProperty('IsPlayable', 'true')
-        url = f"{sys.argv[0]}?action=play&url={urllib.parse.quote(stream_url)}"
-        xbmcplugin.addDirectoryItem(HANDLE, url, item, False)
+        num = ep.get('episodio', '')
+        titulo = ep.get('titulo', '')
+        strm_url = ep.get('strm', '')
+        if not strm_url:
+            continue
+        nombre = f"{num} - {titulo}" if titulo else f"Episodio {num}"
+        item = xbmcgui.ListItem(nombre)
+        item.setInfo('video', {'title': nombre, 'mediatype': 'episode', 'season': int(temp), 'episode': int(num) if num.isdigit() else 0})
+        xbmcplugin.addDirectoryItem(HANDLE, strm_url, item, False)
     xbmcplugin.setContent(HANDLE, 'episodes')
     xbmcplugin.endOfDirectory(HANDLE)
 
-def play(url):
-    item = xbmcgui.ListItem(path=url)
-    xbmcplugin.setResolvedUrl(HANDLE, True, item)
+def main():
+    params = {}
+    if len(sys.argv) > 2:
+        query = sys.argv[2].lstrip('?')
+        if query:
+            for p in query.split('&'):
+                if '=' in p:
+                    k, v = p.split('=', 1)
+                    params[k] = urllib.parse.unquote(v)
 
-params = dict(urllib.parse.parse_qsl(sys.argv[2].lstrip('?')))
-action = params.get('action', '')
+    action = params.get('action', 'menu')
+    if action == 'menu':
+        menu_principal()
+    elif action == 'generos':
+        listar_generos()
+    elif action == 'colecciones':
+        listar_colecciones()
+    elif action == 'series_por_genero':
+        genero = params.get('genero', '')
+        listar_series_por_genero(genero)
+    elif action == 'series_por_coleccion':
+        coleccion = params.get('coleccion', '')
+        listar_series_por_coleccion(coleccion)
+    elif action == 'temporadas':
+        serie = params.get('serie', '')
+        listar_temporadas(serie)
+    elif action == 'episodios':
+        serie = params.get('serie', '')
+        temp = params.get('temp', '')
+        listar_episodios(serie, temp)
 
-if action == '':
-    menu_principal()
-elif action == 'generos':
-    listar_generos()
-elif action == 'colecciones':
-    listar_colecciones()
-elif action == 'series_por_genero':
-    listar_series_por_genero(urllib.parse.unquote(params['genero']))
-elif action == 'series_por_coleccion':
-    listar_series_por_coleccion(urllib.parse.unquote(params['coleccion']))
-elif action == 'temporadas':
-    listar_temporadas(urllib.parse.unquote(params['serie']))
-elif action == 'episodios':
-    listar_episodios(urllib.parse.unquote(params['serie']), params['temporada'])
-elif action == 'play':
-    play(urllib.parse.unquote(params['url']))
+if __name__ == "__main__":
+    main()
